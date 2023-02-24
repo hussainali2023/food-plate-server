b9521362368794fdd2b29fd90d7b997e89475c44
hussain2023 <iamhussainali2@gmail.com>
2023-02-24 22:13:10 +0530

    get data from mondgodb database

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("server is running");
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.user}:${process.env.password}@cluster0.xgu9cba.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
-app.listen(5000, () => {
-  console.log("server is Running on Port 5000");
+async function run() {
+  try {
+    const foodCollection = client
+      .db("food-plate")
+      .collection("foodsCollection");
+
+    app.get("/foods", async (req, res) => {
+      const query = {};
+      const foods = await foodCollection.find(query).toArray();
+      res.send(foods);
+    });
+  } catch (data) {
+    console.log(data);
+  }
+}
+
+run().catch((data) => console.log(data));
+
+app.get("/", async (req, res) => {
+  res.send("Food Plate server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Food Plate server is Running on Port ${port}`);
 });
