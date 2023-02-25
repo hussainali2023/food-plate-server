ca91e79feb66e40979d846c07a0bf8f9a656066c
hussain2023 <iamhussainali2@gmail.com>
2023-02-25 22:23:28 +0530

    show items with id

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
-const port = process.env.PORT || 5000;
-
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -27,6 +26,12 @@ async function run() {
       const query = {};
       const foods = await foodCollection.find(query).toArray();
       res.send(foods);
+    });
+    app.get("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const food = await foodCollection.findOne(query);
+      res.send(food);
     });
   } catch (data) {
     console.log(data);
